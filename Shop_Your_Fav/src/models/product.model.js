e92587eb7a17ed50d3c2203573dd6cdc2d055a97
PrincyKanpariya <134901668+PrincyKanpariya@users.noreply.github.com>
2023-09-04 09:01:44 +0530

    ok

--- Shop_Your_Fav/src/models/product.model.js
+++ Shop_Your_Fav/src/models/product.model.js
@@ -10,6 +10,10 @@ const productSchema = new mongoose.Schema(
             type: String,
             trim: true,
         },
+        category: {
+            type: mongoose.Types.ObjectId,
+            ref: "category",
+        },
         is_active: {
             type: Boolean,
             trim: true,
